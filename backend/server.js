be60b2efd660461cef8e977f88aa55a0b42018c1
itskaramaman <45313619+itskaramaman@users.noreply.github.com>
2022-08-11 18:06:46 -0700

    backend configured

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
-const cors = require("cors");
 
 const app = express();
-app.use(bodyParser.json());
-app.use(cors);
+app.use(bodyParser.urlencoded({ extended: true }));
 connectDB();
 app.use("/api/movies", require("./routes/MovieRoute"));
 
